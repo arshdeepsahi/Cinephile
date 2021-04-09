5f865ef2866d6f10fc639011eac80e45f0c8656f
toladele <tommy.oladele@mail.utoronto.ca>
2021-04-09 00:25:22 -0400

    print search query in console

--- cinephile/src/App.js
+++ cinephile/src/App.js
@@ -17,43 +17,98 @@ const labelOffset = -6
 // or provide it yourself - see notes below
 const focused = 2
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="shake">
-          <img src={logo} className="App-logo" alt="logo" />
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  state = {
+    searchQuery: "",
+    history: [],
+    moviePoster: "",
+    movieTitle: "",
+    movieGenre: "",
+    movieRuntime: "",
+    moviePlot: "",
+    movieYear: "",
+    movieImdbRating: "",
+  };
+
+  updateQuery = () => {
+    // read all entities
+    fetch(
+      `http://www.omdbapi.com/?t=${this.state.searchQuery}&apikey=11a56ba6&`,
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        console.log(response["Title"]);
+        this.setState({
+          moviePoster: response["Poster"],
+          movieTitle: response["Title"],
+          movieGenre: response["Genre"],
+          movieRuntime: response["Runtime"],
+          moviePlot: response["Plot"],
+          movieYear: response["Year"],
+          movieImdbRating: response["imdbRating"],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleChange(event) {
+    this.setState({ searchQuery: event.target.value });
+  }
+
+  handleSubmit(event) {
+    // this.setState({ searchQuery: event.target.value });
+    this.updateQuery();
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <div className="shake">
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+            <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
+              <div>
+                <TextField
+                  // label="Search"
+                  variant="outlined"
+                  /* styles the wrapper */
+                  style={{ height, width}}
+                  /* styles the label component */
+                  InputLabelProps={{
+                    style: {
+                      height,
+                      ...(!focused && { top: `${labelOffset}px` }),
+                    },
+                  }}
+                  /* styles the input component */
+                  inputProps={{
+                    onChange: this.handleChange,
+                      style: {
+                        height,
+                        padding: '0 14px',
+                      },
+                  }}
+              />
+              </div>
+              <input type="submit" value="Submit" />
+            </form>
+          </header>
         </div>
-        <form noValidate autoComplete="off">
-          <div>
-            <TextField
-              label="Search"
-              variant="outlined"
-              
-              /* styles the wrapper */
-              style={{ height, width}}
-
-              /* styles the label component */
-              InputLabelProps={{
-                style: {
-                  height,
-                  ...(!focused && { top: `${labelOffset}px` }),
-                },
-              }}
-
-              /* styles the input component */
-              inputProps={{
-                  style: {
-                    height,
-                    padding: '0 14px',
-                  },
-              }}
-          />
-          </div>
-        </form>
-      </header>
-    </div>
-  );
+      );
+    }
 }
-
-export default App;
---
+++ cinephile/src/components/searchResult.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class SearchResult extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  render() {
+    return <label>second component</label>
+    }
+}
